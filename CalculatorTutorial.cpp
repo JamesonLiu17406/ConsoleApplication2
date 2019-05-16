edb66624c306499530fa42ef31db6a63b3031620
JamesonLiu17406 <JamesonLiu17406@users.noreply.github.com>
2019-05-16 23:52:23 +0800

    I update CalculatorTutorial.cpp file. The update is add a comment to test git.

--- CalculatorTutorial.cpp
+++ CalculatorTutorial.cpp
@@ -1,6 +1,8 @@
 // ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// Add a new comment to test git.
+
 #include <iostream>
 #include "Calculator.h"
 
